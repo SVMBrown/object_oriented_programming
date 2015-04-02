41e29a24bff2dfc98a4630516cc9af0704dc776c
SVMBrown <scotb38@gmail.com>
2015-04-02 14:22:08 -0400

    added name and parent class

--- people.rb
+++ people.rb
@@ -1,6 +1,14 @@
-class Instructor
+class Person
+    def initialize(name)
+        @name = name
+    end
 end
-class Student
+class Instructor < Person
+    def teach
+        "Everything in Ruby is an Object!"
+    end
+end
+class Student < Person
     def learn
         "I get it!"
     end
